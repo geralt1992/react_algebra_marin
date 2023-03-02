4940c09ed74ee13b8604f777344169d16008b2d7
marin sabljo <marin.sabljo@gmail.com>
2023-03-02 20:27:22 +0100

    9 state

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import {UserChildren , UserFunction, UserClass } from './Components';
 
 class App extends React.Component {
 
-  users = [
-      {name: 'iva', age : 25},
-      {name: 'marin', age : 33},
-      {name: 'nina', age : 53},
-  ]
+  state = {
+    users : [
+        {name: 'iva', age : 25},
+        {name: 'marin', age : 33},
+        {name: 'nina', age : 53},
+    ]
+  }
 
  render() {
+
+  const { users } = this.state;
+
+
   return
     <div>
-      <UserChildren> Moje ime je {this.users[2].name} i imam {this.users[2].age} godina </UserChildren>
-      <UserFunction user={this.users[1]}/>
-      <UserClass user={this.users[2]}/>
+      <UserChildren> Moje ime je {users[2].name} i imam {this.users[2].age} godina </UserChildren>
+      <UserFunction user={users[1]}/>
+      <UserClass user={users[2]}/>
     </div>
   
   
