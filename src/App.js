225e41cb24304b14d1dfc9aa71d26d7c36d2e772
marin sabljo <marin.sabljo@gmail.com>
2023-03-04 19:34:52 +0100

    funkcija

--- src/App.js
+++ src/App.js
@@ -2,39 +2,24 @@ import './App.css';
 import React from 'react';
 import { UserFunction } from './Components';
 
-class App extends React.Component {
+function App()  {
 
-  state = {
-    user :  {name: 'marin', age : 33},
-  }
-
+  const [user, setUser] = React.useState( {name: 'marin', age : 33});
 
 //FORA ZA PRENJETI DATA IZ CHILDA U PARENT
 //ZA UPDEJTANJE STEJTA
 
 //stejt nikad ne diras, moras ga rekreirati!
 
-  handleNameChange = (event) => {
+  const handleNameChange = (event) => {
     let value = event.target.value;
-
-    //dobor je dikonsturktat jer dodjeliš "const" objektima i poljima, koji trebaju const inace uz sebe jer su nepromijenjivi
-    const { user } = this.state;
-
     const newUser =  { ...user, name : value};
-
-    this.setState({user : newUser});
+    setUser(newUser);
   }
 
-
- render() {
-  const { user } = this.state;
-
   return <div> 
-    <UserFunction user={user} handleNameChange={this.handleNameChange}/> 
-  
+    <UserFunction user={user} handleNameChange={handleNameChange}/> 
   </div>
-  
- }
  
 }
 
