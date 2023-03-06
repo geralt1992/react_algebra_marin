d9b5ae00578a686f38d2a7fdb1fe59c5d4acf9ab
marin sabljo <marin.sabljo@gmail.com>
2023-03-06 20:44:35 +0100

    forme u reactu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  //ovo '' u useState je početno stanje stejta "name"
+  //tezi uvijek da sve sto korinsik unosi da povezujes sa stejtom
+ const [name, setName] = React.useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setName('');
+    alert(name);
+   
+  }
+
+
+  function handleNameChange(e) {
+    let value = e.target.value; 
+    setName(value);
+  }
+
   return (
-    <div>
-      
-    </div>
+    <>    
+    <form onSubmit={handleSubmit}>
+    <label>
+      Ime:
+      <input onChange={handleNameChange} value={name} type={"text"}></input>
+    </label>
+    <input type={"submit"} value={"Potvrdi"}></input>
+    </form>
+    </>
+
   );
+
 }
 
 export default App;
