e9937fe77de4f4c7afb5b03b6ec8dd004b672783
marin sabljo <marin.sabljo@gmail.com>
2023-03-09 18:41:13 +0100

    s

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 
+//ovo je hoc koja ce moci prosljediti color / prima komponentu
+function withColor(Component) {
+  return function() {
+    return <Component/>
+  }
+}
+
+function Button() {
+  return <button>Mali Gumb</button>
+}
+
+const ColorButton = withColor(Button);
+
 function App() {
   return (
     <div>
-      
+        <ColorButton />
     </div>
   );
 }
