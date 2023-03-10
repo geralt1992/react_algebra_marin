87ecd73fdd66e1104fb60e1206e54d0bf55b3311
marin sabljo <marin.sabljo@gmail.com>
2023-03-10 19:22:41 +0100

    API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+import { useEffect } from 'react';
 import './App.css';
+import React from 'react';
 
+//ZVATI API PREKO USEEFFECT HOOKA i SPOJITI SA STATOM
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+
+  const [dataGettedFromServer , setData] = React.useState({});
+
+  useEffect(() => {
+    
+    fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      setData(data);
+    }) 
+
+  }, []);
+
+
+
+
+  if(!dataGettedFromServer) {
+    return 'Loding NASA data...';
+  }
+  else {
+    // const [title , url , explanation] = dataGettedFromServer;
+    return (
+      <div>
+        <h3>{dataGettedFromServer.title}</h3>
+        <img src={dataGettedFromServer.url} alt="opis_u_slucaju_da_slika_nije_ucitana" />
+        <p>{dataGettedFromServer.explanation}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
