6d7b3a4b4e91205c86b926659a14b94fe3f80c24
marin sabljo <marin.sabljo@gmail.com>
2023-03-12 13:36:53 +0100

    d

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import { gql , useQuery } from '@apollo/client';
+
 
 function App() {
+
+  const { loading , error, data } = useQuery(gql`
+    {
+      rates(currency: "USD") {
+        currency
+        rate
+      }
+    } 
+  `);
+
+  if(loading) return <p>Loading...</p>
+  if(error) return <p>{error.message}</p>
+
+
   return (
-    <div>
-      
-    </div>
+    <ul>
+      {data.rates.map((rate, index) => {
+        return <li key={index}>{rate.currency}: {rate.rate}</li>
+      })}
+    </ul>
   );
 }
 
